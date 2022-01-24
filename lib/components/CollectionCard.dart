42dff7b0aa1dd0cf586d0d5e694d3fb80693b347
Oguz Vuruskaner <ovuruska@outlook.com>
2022-01-25 00:05:25 +0300

    Implemented CollectionCard.

--- lib/components/CollectionCard.dart
+++ lib/components/CollectionCard.dart
@@ -10,67 +10,63 @@ class CollectionCard extends StatelessWidget {
     return Center(
 
       child:Container(
+        width:screenWidth*.75,
         child: Card(
+
           shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15.0)),
-          color:Color(0xFF161853),
+            borderRadius: BorderRadius.circular(8.0)
+          ),
+          color:Color(0xFF4B4385),
           child:Column(
             children:[
               Container(
-                  margin:EdgeInsets.only( left:32,top:32,right:32,bottom:8),
-                  child:Image.asset(
-                  p.join("assets","images","oguz.png"),
-                  width:screenWidth*.5,
-                  height:screenWidth*.5,
-              ))
-              ,
-              Text("They Live",
-                style: TextStyle(fontSize:32,color:Color(0xFFFFFFFF))
-              ),
-              Container(
-                  width:screenWidth*.5,
-                  child:Row(
+                  margin:EdgeInsets.all( 0),
 
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children:[Container(
-                        margin:EdgeInsets.only( top:16,bottom:16),
-                      child:Text(
-                      "by Yagiz Eyiisleyen",
-                      style: TextStyle(fontSize:16,color:Color(0xFFFFFFFF))
-                      )),
-                      Container(
-                        alignment: Alignment.center,
-                        margin:EdgeInsets.only( ),
-                        child:Image.asset(
-                          p.join("assets","images","yagiz.png"),
-                          width:24,
-                          height:24,
-                        ))
-                  ]
-              )),
-              Container(
-                  margin:EdgeInsets.only( top:16,bottom:16),
-                  child:TextButton(
-                    style:TextButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      padding: EdgeInsets.symmetric(horizontal: 32, vertical: 8),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18.0),
-                          side: BorderSide(color: Colors.red)
-                      )
-                    ),
-                    onPressed: () {
-                      Navigator.pushNamed(context,"/UploadYourImage");
-                    },
-                    child:Text(
-                      "Order Now",
-                    style: TextStyle(fontSize:24,color:Color(0xFF161853))
-                  )
-              ))
-            ]
-          )
-        )
-      )
-    );
+                  child:ClipRRect(
+                    borderRadius:BorderRadius.circular(8),
+                    child:Image.asset(
+                      //TODO: cardImage will be variable.
+                      p.join("assets","images","oguz.png"),
+                      width:screenWidth*.75,
+                      height:screenWidth*.75,
+                    )
+                  )),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children:[
+                  Container(
+                    child:Text("They Live",style: TextStyle(fontSize:24,color:Color(0xFFFFFFFF))),
+                    margin:EdgeInsets.only( left:8),
+
+
+                  ),
+                Container(
+                    height:48,
+                    margin:EdgeInsets.only(right:8 ),
+
+                    child:Row(
+
+                        children:[
+                          Container(
+                            margin:EdgeInsets.only( right:4 ),
+                            child:Text(
+                        "by Yagiz Eyiisleyen",
+                        style: TextStyle(fontSize:12,color:Color(0xFFFFFFFF))
+                        )),
+                        Container(
+                          alignment: Alignment.center,
+                          margin:EdgeInsets.only( ),
+                          child:ClipRRect(
+                              borderRadius:BorderRadius.circular(8),
+                              child:Image.asset(
+                                //TODO: cardImage will be variable.
+                                p.join("assets","images","yagiz.png"),
+                                width:16,
+                                height:16,
+                              )
+                          ))
+                      ]))
+                ])
+              ]))));
   }
 }
